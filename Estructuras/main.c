70e4da6118512180d0aa951edf6a33bc3a0795c1
Di Filippo Leandro <leeandifi@gmail.com>
2018-04-16 11:32:35 -0300

    Clase8 (estructuras)

---
+++ Estructuras/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    char nombre[20];
+    int legajo;
+    float sueldo;
+    char sexo;
+}eEmpleado;
+
+int main()
+{
+
+
+
+    return 0;
+}
